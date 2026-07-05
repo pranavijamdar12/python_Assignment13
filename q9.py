61f295da70cdfb4e29faf6e4fe9e2e0dc1fdef26
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-05 11:04:46 +0530

    Implement addition function in q9.py

---
+++ q9.py
@@ -0,0 +1,12 @@
+Addition = lambda no1,no2:(no1 + no2)
+
+def main():
+    no1 = int(input("Enter the number you want:"))
+    no2 = int(input("Enter the number you want:"))
+
+    ret = Addition(no1,no2)
+    print("The value is :",ret)
+
+if __name__ == "__main__":
+    main()
+    

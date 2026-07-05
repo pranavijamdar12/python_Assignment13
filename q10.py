741cd0d4cf64a76fb1660553ac3c64f73c2d75fd
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-05 11:12:49 +0530

    Add multiplication function and main execution

---
+++ q10.py
@@ -0,0 +1,13 @@
+Multipliaction = lambda no1,no2:(no1 * no2)
+
+def main():
+    no1 = int(input("Enter the number you want:"))
+    no2 = int(input("Enter the number you want"))
+
+    ret = Multipliaction(no1,no2)
+    print("The value is :",ret)
+
+
+if __name__ == "__main__":
+    main()
+    

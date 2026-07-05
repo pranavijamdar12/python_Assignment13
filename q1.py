85522262f962fcb247de545e2036f9943dc2ea66
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-05 10:18:44 +0530

    Add square calculation functionality in q1.py

---
+++ q1.py
@@ -0,0 +1,9 @@
+Square = lambda no:(no*no)
+def main():
+    no = int(input("Enter the number you want:"))
+    ret = Square(no)
+    print("The square is :",ret)
+
+
+if __name__ == "__main__":
+    main()

0bfcccba075be81e51d16cfefa1a1f820def79ee
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-05 10:40:38 +0530

    Implement minimum number finder in q5.py

---
+++ q5.py
@@ -0,0 +1,8 @@
+Min  = lambda no1,no2:(no1 if no1 < no2 else no2)
+def main():
+    no1 = int(input("Enter the number you want:"))
+    no2 = int(input("Enter the number you want:"))
+    ret = Min(no1,no2)
+    print("The Minimum number is :",ret)
+if __name__ == "__main__":
+    main()

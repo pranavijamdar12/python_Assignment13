508b9f898a3629c69889b8ec86554fd295fe063d
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-05 10:22:57 +0530

    Add cube calculation functionality in q2.py

---
+++ q2.py
@@ -0,0 +1,11 @@
+
+Cube = lambda no:(no*no*no)
+def main():
+    no = int(input("Enter the number you want:"))
+    ret = Cube(no)
+    print("The cube of number is :",ret)
+
+
+
+if __name__ == "__main__":
+    main()   
